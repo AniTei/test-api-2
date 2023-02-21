4af6bf344ca6f2e5fceb76818715d7542f396524
AniTei <115176414+AniTei@users.noreply.github.com>
2023-02-21 22:38:22 +0100

    ferdig for idag

--- js/details.js
+++ js/details.js
@@ -1,31 +1,13 @@
 const detContDiv = document.querySelector(".details-content");
-const detailsTitle = document.querySelector (".details-title");
-
-
+const detailsTitle = document.querySelector(".details-title");
 
 const queryString = document.location.search;
-
 const params = new URLSearchParams(queryString);
-
 const idDrink = params.get("id");
 
 console.log(idDrink);
 
-
-
-
-
-
-const url = "htttps://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + idDrink;
-
-//, I have hardcoded a specific drink into the url, now I need to pass the idDrink into it
-
-// use target to send the property from the click to the new url?
-
-//const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php  ?   i=17216" + idDrink;
-
-//const idDrink = querystring osv
-
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + idDrink;
 
 async function findDrink() {
     try {
@@ -33,37 +15,25 @@ async function findDrink() {
         const data = await respond.json();
         console.log(data);
 
-
+        detContDiv.innerHTML = "";
 
         const yourDrink = data.drinks[0]
 
         detContDiv.innerHTML =
-        `<div>
-            <img src="${yourDrink.strDrinkThumb}" alt = "serving example"/>
-            <div> ${yourDrink.strDrink}</div>
-            <div> 
-                <p>Main ingredients: ${yourDrink.strIngredient1}, ${yourDrink.strIngredient2}, ${yourDrink.strIngredient3} </p>
-                <p>Instructions: ${yourDrink.strInstructions}</p>
-            </div>
-        </div>`
+            `<div class="upper-details-container">
+            <h2 class="details-name upper-details-item"> ${yourDrink.strDrink}</h2>
+            <img class="details-img upper-details-item" src="${yourDrink.strDrinkThumb}" alt = "serving example"/>
+            <p>Main ingredients: ${yourDrink.strIngredient1}, ${yourDrink.strIngredient2}, ${yourDrink.strIngredient3} </p>
+            <p>Instructions: ${yourDrink.strInstructions}</p>
+            </div>`
 
         detailsTitle.innerHTML = `${yourDrink.strDrink}`;
 
-
-
     }
     catch (error) {
-
-
-
-
         console.log("OOOPSIE:/sjekk internettforbindelse!!!", error);
-
         detContDiv.innerHTML = `<div class="error">OH NO, something went wrong 😢</div>`;
-
-        
     }
 }
 
 findDrink();
-
--- js/script.js
+++ js/script.js
@@ -1,74 +1,36 @@
 
 const contentDiv = document.querySelector(".content-container");
 
-
-// files successfully connected
-// call API
-
-
-
-
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita";
-
-
 
 async function getSomeContent() {
     try {
         const respond = await fetch(url);
         const data = await respond.json();
-
         console.log(data.drinks);
 
-        
+        contentDiv.innerHTML = "";
 
-        //loop needed for data.results[i]
         for (let i = 0; i < data.drinks.length; i++) {
 
-            console.log(data.drinks[i])
+            console.log(data.drinks[i]);
 
             contentDiv.innerHTML += `<a href="details.html?id=${data.drinks[i].idDrink}"> 
-                                    <div class="user-item-index">
-    
-                                         <div class="username-index"> ${data.drinks[i].strDrink}</div>
-                                         <div> This drink is ${data.drinks[i].strAlcoholic}</div>
-                                         <img class="listed-img" src="${data.drinks[i].strDrinkThumb}" alt= "serving example"/>
-                                         
-                                            </div>
-                                </a>`;
+                                        <div class="item-index">
+                                            <h2 class="name-index"> ${data.drinks[i].strDrink}</h2>                                       
+                                            <img class="listed-img" src="${data.drinks[i].strDrinkThumb}" alt= "serving example"/>
+                                            <div> This drink is ${data.drinks[i].strAlcoholic}</div>
+                                        </div>
+                                    </a>`;
         }
-
-        //displaying username, name and 
-        //displayContent(data);
 
     }
     catch (error) {
         console.log("OOOPSIE:/sjekk internettforbindelse!!!", error);
-
         contentDiv.innerHTML = `<div class="error">OH NO, something went wrong 😢</div>`;
-
     }
 }
 getSomeContent();
-
-/* function createHtml (para) {
-}
- */
-
-
-/*
-function displayContent(person) {
-    contentDiv.innerHTML = `<div> my gender is: ${data.results[0].gender}</div>`;
-}
- */
-
-//got a user!!!
-
-
-//////////////////////
-
-
-
-
 
 
 
